bbb89968dc543fb0c8bd4be84d152f9f61f73785
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-18 14:31:49 -0700

    adds solution with test

--- sequential-bi-grams.rb
+++ sequential-bi-grams.rb
@@ -5,16 +5,27 @@
 # Keep performance & memory usage in mind
 # Include tests
 
-strings = "Make a killer impression on whoever you're meeting."
-strings = strings.split(" ")
-a = 0
-i = 1
+def make_bi_gram(strings)
+  strings = strings.split(" ")
+  a = 0
+  i = 1
 
-while i < strings.length
-  p "#{strings[a]} #{strings[i]}"
-  i += 1
-  a += 1
+  while i < strings.length
+    p "#{strings[a]} #{strings[i]}"
+    i += 1
+    a += 1
+  end
 end
-
 # Tests #
 
+# Test 1 #
+# make_bi_gram("Make a killer impression on whoever you're meeting.")
+
+# Test 2 #
+# make_bi_gram("Testing code here")
+
+# Test 3 #
+# make_bi_gram("One does not simply walk into Mordor. Its black gates are guarded by more than just orcs. There is evil there that does not sleep. And the Great Eye, is ever watchful. It is a barren wasteland, riddled with fire, ash and dust. The very air you breathe is a poisonous fume. Not with ten thousand men could you do this. It is folly!")
+
+# Test 4 #
+# make_bi_gram("These aren’t the droids you’re looking for.")
